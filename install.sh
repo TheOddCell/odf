2d1aff14b1bba8fa954cf810854b5d55bb69f57d
TheOddCell <odd@obsidianos.xyz>
2025-10-05 18:03:58 -0400

    installl and update

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+[ "$(id -u)" -eq 0 ] || (echo "root please"; exit 1)
+
+git clone https://github.com/theoddcell/odf /usr/share/odf
+ln -s /usr/share/odf/odf /usr/local/bin/odf
+echo "installed\! Run /usr/share/odf/update.sh as root to update odf."
+exec /usr/local/bin/odf
+
---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+[ "$(id -u)" -eq 0 ] || (echo "root please"; exit 1)
+
+(cd /usr/share/odf;git pull)
+echo "updated\!"
+exec /usr/local/bin/odf
+
