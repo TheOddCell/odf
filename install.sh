d34c60738bf3a8ac227bc737256f5fdd3e3f383d
/home/odd <odd@obsidianos.xyz>
2025-10-05 20:52:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 [ "$(id -u)" -eq 0 ] || (echo "root please"; exit 1)
 
 git clone https://github.com/theoddcell/odf /usr/share/odf
+(cd /usr/share/odf; git pull)
 ln -s /usr/share/odf/odf /usr/local/bin/odf
 echo "installed! Run /usr/share/odf/update.sh as root to update odf."
 exec /usr/local/bin/odf
